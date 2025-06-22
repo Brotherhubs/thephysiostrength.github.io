2c7fd542df6b7fc81ddd0d0f9f180b2891ccb615
Azeem Chauhan <azeemchauhan@gmail.com>
2025-06-22 13:14:09 +0530

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,33 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/thephysiostrength.github.io",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+
   distDir: 'dist',
 };
+
 
 export default nextConfig;
--- src/data/hero-slides.ts
+++ src/data/hero-slides.ts
@@ -4,25 +4,25 @@ import { HeroSlideData } from '../types';
 export const heroSlidesData: HeroSlideData[] = [
   {
     id: 'slide1',
-    imageUrl: '/images/transcutaneius-therapy.jpeg',
+    imageUrl: '/thephysiostrength.github.io/images/transcutaneius-therapy.jpeg',
     title: 'Advanced TENS Therapy',
     description: 'Experience targeted pain relief with our advanced Transcutaneous Electrical Nerve Stimulation (TENS) therapy.',
   },
   {
     id: 'slide2',
-    imageUrl: '/images/Cupping-therapy.jpeg',
+    imageUrl: '/thephysiostrength.github.io/images/Cupping-therapy.jpeg',
     title: 'Holistic Cupping Therapy',
     description: 'Enhance circulation and relieve muscle tension with our traditional and effective cupping therapy sessions.',
   },
   {
     id: 'slide3',
-    imageUrl: '/images/Ultrasonic-therapy.jpeg',
+    imageUrl: '/thephysiostrength.github.io/images/Ultrasonic-therapy.jpeg',
     title: 'Innovative Ultrasonic Therapy',
     description: 'Accelerate healing and reduce inflammation with our state-of-the-art ultrasonic therapy treatments.',
   },
   {
     id: 'slide4',
-    imageUrl: '/images/medical-assistant-helping-patient-with-physiotherapy-exercises.jpg',
+    imageUrl: '/thephysiostrength.github.io/images/medical-assistant-helping-patient-with-physiotherapy-exercises.jpg',
     title: 'Personalized Exercise Programs',
     description: 'Regain strength and mobility with guided exercise plans tailored to your specific needs and goals.',
   }
